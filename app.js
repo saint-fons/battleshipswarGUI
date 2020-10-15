cb3cc401fa92f4e09833d4b7ba3f14213c055fbd
IvanZhuravlev <i.zhuravlev.work@gmail.com>
2020-10-15 16:05:53 +0300

    finished. work fine

--- app.js
+++ app.js
@@ -34,11 +34,9 @@ let model = {
     shipLength: 3,
     shipsSunk: 0,
 
-    ships: [
-        { locations: ["06", "16", "26"], hits: ["", "", ""] },
-        { locations: ["24", "34", "44"], hits: ["", "", ""] },
-        { locations: ["10", "11", "12"], hits: ["", "", ""] }
-    ],
+    ships: [ { locations: [0, 0, 0], hits: ["", "", ""] },
+             { locations: [0, 0, 0], hits: ["", "", ""] },
+             { locations: [0, 0, 0], hits: ["", "", ""] } ],
 
     fire: function(guess) {
         for (let i = 0; i < this.numShips; i++) {
@@ -69,7 +67,53 @@ let model = {
             }
         }
         return true;
+    },
+    generateShipLocations: function() {
+        let locations;
+        for (let i = 0; i < this.numShips; i++) {
+            do {
+                locations = this.generateShip();
+            } while (this.collision(locations));
+            this.ships[i].locations = locations;
+        }
+        console.log("Ships array: ");
+        console.log(this.ships);
+    },
+    generateShip: function () {
+        let direction = Math.floor(Math.random() * 2)
+
+        let row, col;
+
+        if (direction === 1) {
+            row = Math.floor(Math.random() * this.boardSize);
+            col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+        } else {
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+            col = Math.floor(Math.random() * this.boardSize);
+        }
+
+        let newShipLocations = [];
+        for(let i = 0; i < this.shipLength; i++) {
+            if (direction === 1) {
+                newShipLocations.push(row + "" + (col + i));
+            } else {
+                newShipLocations.push((row + i) + "" + col);
+            }
+        }
+        return newShipLocations;
+    },
+    collision: function(locations) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = model.ships[i];
+            for (let j = 0; j < locations.length; j++) {
+                if (ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
+
 };
 
 let controller = {
@@ -89,7 +133,7 @@ function parseGuess(guess) {
     if (guess === null || guess.length !== 2) {
         alert("Oops, please enter a letter and a number on the board.");
     } else {
-        firstChar = guess.charAt(0);
+        let firstChar = guess.charAt(0);
         let row = alphabet.indexOf(firstChar);
         let column = guess.charAt(1);
         if (isNaN(row) || isNaN(column)) {
@@ -104,7 +148,63 @@ function parseGuess(guess) {
     return null;
 }
 
-console.log(parseGuess("A0"));
+function handleFireButton() {
+    let guessInput = document.getElementById("guessInput")
+    let guess = guessInput.value;
+    controller.processGuess(guess);
+
+    guessInput.value = "";
+}
+
+
+function handleKeyPress(e) {
+    let fireButton = document.getElementById("fireButton");
+    if (e.keyCode === 13) {
+        fireButton.click()
+        return false;
+    }
+}
+
+
+window.onload = init;
+
+
+function init() {
+    // Fire! button onclick handler
+    let fireButton = document.getElementById("fireButton");
+    fireButton.onclick = handleFireButton;
+
+    // handle "return" key press
+    let guessInput = document.getElementById("guessInput");
+    guessInput.onkeypress = handleKeyPress;
+
+    // place the ships on the game board
+    model.generateShipLocations();
+}
+
+
+
+
+
+
+
+
+
+/*controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");*/
+
+
+
+
+/*console.log(parseGuess("A0"));
 console.log(parseGuess("B6"));
 console.log(parseGuess("G3"));
 console.log(parseGuess("H0"));
@@ -122,6 +222,6 @@ model.fire("24");
 model.fire("44");
 model.fire("12");
 model.fire("11");
-model.fire("10");
+model.fire("10");*/
 
 view.displayMessage("Tap tap, is this thing on?");
